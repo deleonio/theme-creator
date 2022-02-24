2cb40f06b986b67c9fa63b7a08f68f9ffe32b131
Martin Oppitz <github@martinoppitz.com>
2022-02-24 03:30:14 +0100

    chore: working theming

--- src/components/app/component.solid.tsx
+++ src/components/app/component.solid.tsx
@@ -2,10 +2,58 @@ import { Component, createSignal } from 'solid-js';
 
 import { KolInputText, KolSelect } from '@kolibri/solid';
 import { EditorComponent } from '../editor/component.solid';
+import { KoliBriDevHelper } from '@kolibri/lib';
 
 export const AppComponent: Component = () => {
   const [getTheme, setTheme] = createSignal<string>('default');
   const [getComponent, setComponent] = createSignal<string>('KOL-BUTTON');
+
+  KoliBriDevHelper.patchKoliBriTheme(
+    getTheme(),
+    'KOL-BADGE',
+    `:host span {
+  border-radius: 2em;
+  padding: 0.5em 1em;
+}`
+  );
+  KoliBriDevHelper.patchKoliBriTheme(
+    getTheme(),
+    'KOL-BUTTON',
+    `:host button {
+    border-radius: 2em;
+    padding: 0.5em 1em;
+  }
+  :host button.primary {
+    background-color: blueviolet;
+  }
+  :host button.primary:hover {
+    background-color: white;
+  }
+  :host button.secondary {
+    background-color: rgb(165, 89, 236);
+  }
+  :host button.secondary:hover {
+    background-color: white;
+  }
+  :host button.normal {
+    background-color: rgb(43, 153, 226);
+  }
+  :host button.normal:hover {
+    background-color: white;
+  }
+  :host button.danger {
+    background-color: red;
+  }
+  :host button.danger:hover {
+    background-color: white;
+  }
+  :host button.ghost {
+    background-color: white;
+  }
+  :host button.ghost:hover {
+    background-color: gray;
+  }`
+  );
 
   return (
     <div class="font-sans grid">
@@ -39,14 +87,14 @@ export const AppComponent: Component = () => {
           _value={[getComponent()]}
           _on={{
             onChange: (_event, value) => {
-              setComponent((value as string[])[0]);
+              setComponent((value as KoliBri.TagName[])[0]);
             },
           }}
         >
           Komponenten
         </KolSelect>
       </div>
-      <EditorComponent component={getComponent()} theme={getTheme()}></EditorComponent>
+      <EditorComponent tagName={getComponent()} theme={getTheme()}></EditorComponent>
     </div>
   );
 };
--- src/components/editor/component.solid.tsx
+++ src/components/editor/component.solid.tsx
@@ -1,10 +1,10 @@
-import { Component, createEffect, createSignal, on } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 import { createCssEditor } from './css-editor';
 import { components } from './store';
 
 type Props = {
-  component: string;
+  tagName: string;
   theme: string;
 };
 
@@ -15,30 +15,36 @@ export const EditorComponent: Component<Props> = (props: Props) => {
     setShow(() => true);
   }, 0);
 
-  const Component = () => components[props.component].html;
+  const PreviewComponent = () => components[props.tagName].html;
+
+  const renderEditor = (ref: HTMLElement) => {
+    createCssEditor(ref, props.tagName, props.theme, setShow);
+  };
 
   return (
     <div class="grid grid-cols-2 gap-2 items-center content-center h-80vh m-1">
-      <div class="h-80vh">
-        <div
-          ref={(ref) => createCssEditor(ref, props.component, props.theme, setShow)}
-          style={{
-            height: '100%',
-            width: '100%',
-          }}
-        ></div>
-        {/* <div
+      {getShow() && (
+        <>
+          <div class="h-80vh">
+            <div
+              ref={renderEditor}
+              style={{
+                height: '100%',
+                width: '100%',
+              }}
+            ></div>
+            {/* <div
           ref={(ref) => createHtmlEditor(ref, props.component)}
           style={{
             height: '33%',
             width: '100%',
           }}
         ></div> */}
-      </div>
-      {getShow() && (
-        <div class="text-center" data-theme={props.theme}>
-          <Component />
-        </div>
+          </div>
+          <div class="text-center" data-theme={props.theme}>
+            <PreviewComponent />
+          </div>
+        </>
       )}
     </div>
   );
