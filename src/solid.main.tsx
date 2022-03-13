1148f97d3d64bebfe8ecde56c11443d3ff93378d
Martin Oppitz <martin.oppitz@itzbund.de>
2022-03-14 00:20:00 +0100

    cleanup project

--- src/solid.main.tsx
+++ src/solid.main.tsx
@@ -1,18 +1,11 @@
 import { render } from 'solid-js/web';
 
-import * as PACKAGE_JSON from '../node_modules/solid-js/package.json';
-import { run } from './app.run';
 import { AppComponent } from './components/app/component.solid';
-import { typeIt } from './shares/utils';
 import { defineCustomElements } from '@kolibri/lib/dist/loader';
 
-const TYPED_PACKAGE_JSON = typeIt<{ version: string }>(PACKAGE_JSON);
-
 defineCustomElements().finally(() => {
-  run('Solid', TYPED_PACKAGE_JSON.version, () => {
-    const htmlDivElement: HTMLDivElement | null = document.querySelector('div#solid');
-    if (htmlDivElement instanceof HTMLDivElement) {
-      render(() => <AppComponent />, htmlDivElement);
-    }
-  });
+  const htmlDivElement: HTMLDivElement | null = document.querySelector('div#solid');
+  if (htmlDivElement instanceof HTMLDivElement) {
+    render(() => <AppComponent />, htmlDivElement);
+  }
 });
