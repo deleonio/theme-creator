63d27c4f20dbb5e3d43f98272bb132bd0dba18bb
Martin Oppitz <github@martinoppitz.com>
2022-04-08 09:49:42 +0200

    chore: update styles

--- src/solid.main.tsx
+++ src/solid.main.tsx
@@ -4,7 +4,13 @@ import { AppComponent } from './components/app/component.solid';
 import { defineCustomElements } from '@kolibri/lib/dist/loader';
 import { register } from '@kolibri/lib';
 
-register(['bmf', 'itz', 'mapz'], defineCustomElements).finally(() => {
+register(['bmf', 'itz', 'mapz'], defineCustomElements, {
+  theme: {
+    cache: true,
+    name: 'mapz',
+    reset: true,
+  },
+}).finally(() => {
   const htmlDivElement: HTMLDivElement | null = document.querySelector('div#solid');
   if (htmlDivElement instanceof HTMLDivElement) {
     render(() => <AppComponent />, htmlDivElement);
