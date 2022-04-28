08f6bbbd2fa9127dd87db1b475cd371328f6d6ca
Martin Oppitz <martin.oppitz@itzbund.de>
2022-04-28 09:26:27 +0200

    chore: configure editorconfig and prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,6 @@
-module.exports = require('@leanup/stack/prettier.config');
+module.exports = {
+	...require('@leanup/stack/prettier.config'),
+	printWidth: 160,
+	singleQuote: true,
+	useTabs: true,
+};
