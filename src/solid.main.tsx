a0de4ce43712491506136633a626abe7164d241c
Martin Oppitz <github@martinoppitz.com>
2022-05-26 05:07:49 +0200

    chore: refactor register theme

--- src/solid.main.tsx
+++ src/solid.main.tsx
@@ -3,8 +3,12 @@ import { render } from 'solid-js/web';
 import { AppComponent } from './components/app/component.solid';
 import { defineCustomElements } from '@kolibri/lib/dist/loader';
 import { register } from '@kolibri/lib';
+import { themeBMF } from '@kolibri/theme-bmf';
+import { themeBPA } from '@kolibri/theme-bpa';
+import { themeITZBund } from '@kolibri/theme-itzbund';
+import { themeMAPZ } from '@kolibri/theme-mapz';
 
-register(['bmf', 'itz', 'mapz'], defineCustomElements, {
+register([themeBMF, themeBPA, themeITZBund, themeMAPZ], defineCustomElements, {
 	theme: {
 		detect: 'auto',
 	},
